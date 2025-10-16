09c7c8c3a4a620d464e5bb7bb5904673cf1cb3e6
Lndvanegas <229024196+Lndvanegas@users.noreply.github.com>
2025-10-15 21:04:52 -0500

    implemente los calculos de los perimetros de las figuras [ cuadrado, circulo, poligono, cuadrado ]

---
+++ calculadora_matematica/superficies/pTriangulo.py
@@ -0,0 +1,6 @@
+def pTriangulo(longitud):
+    return longitud * 3
+longitud = float(input("Ingrese el valor de la longitud de un lado : "))
+
+p = longitud * 3
+print(f"El perímetro del triangulo es: {p:.1f} cm")
